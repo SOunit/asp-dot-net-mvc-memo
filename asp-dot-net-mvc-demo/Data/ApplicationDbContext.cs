6a7eb680012713251d7628c2f2e6d98584220e48
SOunit <workbookwork7@gmail.com>
2022-10-01 12:50:45 -0700

    add record table

---
+++ asp-dot-net-mvc-demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using asp_dot_net_mvc_demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp_dot_net_mvc_demo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // general setting for db-context
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        // create table
+        public DbSet<Record> Tasks { get; set; }
+    }
+}
---
+++ asp-dot-net-mvc-demo/Models/Record.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace asp_dot_net_mvc_demo.Models
+{
+    public class Record
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
