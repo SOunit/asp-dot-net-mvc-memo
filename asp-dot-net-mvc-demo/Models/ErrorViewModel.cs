5320dc41dbe74f877c8a31dd83f539cf23e51848
SOunit <workbookwork7@gmail.com>
2022-10-01 10:23:20 -0700

    Add project files.

---
+++ asp-dot-net-mvc-demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp_dot_net_mvc_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
