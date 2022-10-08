f01f4d8109bf3ddffd797e70e48f1082d870d702
SOunit <workbookwork7@gmail.com>
2022-10-08 10:19:49 -0700

    add razor page

---
+++ asp-dot-net-mvc-demo/Controllers/RazorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_dot_net_mvc_demo.Controllers
+{
+	public class RazorPageController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
