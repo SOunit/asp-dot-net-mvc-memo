34680a575ac40ea26bb9c9da1e34ec87c2abe514
SOunit <workbookwork7@gmail.com>
2022-10-08 09:15:33 -0700

    add update page

---
+++ asp-dot-net-mvc-demo/Data/ViewModels/UpdateOrderVM.cs
@@ -0,0 +1,9 @@
+﻿namespace asp_dot_net_mvc_demo.Data.ViewModels
+{
+    public class UpdateOrderVM
+    {
+        public int OrderId { get; set; }
+        public int OrderItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
