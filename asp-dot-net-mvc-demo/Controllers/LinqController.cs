d1dd70dba6bb653298637f86e1d0caf82f95c547
SOunit <workbookwork7@gmail.com>
2022-10-13 23:11:44 -0700

    add where sample

---
+++ asp-dot-net-mvc-demo/Controllers/LinqController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using asp_dot_net_mvc_demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_dot_net_mvc_demo.Controllers
+{
+    public class LinqController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> productList = new List<Product>()
+            {
+                new Product
+                {
+                    Name = "Test1"
+                },
+                new Product
+                {
+                    Name = "Test2"
+                },
+                new Product
+                {
+                    Name = "Test3"
+                },
+                new Product
+                {
+                    Name = "Test4"
+                },
+            };
+
+            var result = productList.Where(prd => prd.Name == "Test1");
+
+            return View(result);
+        }
+    }
+}
