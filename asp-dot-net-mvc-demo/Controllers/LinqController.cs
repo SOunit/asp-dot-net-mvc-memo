be48c218fc8249e0a6a3694027f344785a154838
SOunit <workbookwork7@gmail.com>
2022-10-14 21:26:53 -0700

    add linq sample

--- asp-dot-net-mvc-demo/Controllers/LinqController.cs
+++ asp-dot-net-mvc-demo/Controllers/LinqController.cs
@@ -1,37 +1,41 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using asp_dot_net_mvc_demo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace asp_dot_net_mvc_demo.Controllers
 {
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Job { get; set; }
+        public double Salary { get; set; }
+    }
+
+    public class LinqVM
+    {
+        public IEnumerable<Employee> EmployeeList { get; set; }
+    }
+
     public class LinqController : Controller
     {
         public IActionResult Index()
         {
-            List<Product> productList = new List<Product>()
+            var vm = new LinqVM();
+
+            List<Employee> employeeList = new List<Employee>()
             {
-                new Product
-                {
-                    Name = "Test1"
-                },
-                new Product
-                {
-                    Name = "Test2"
-                },
-                new Product
-                {
-                    Name = "Test3"
-                },
-                new Product
-                {
-                    Name = "Test4"
-                },
+                new Employee {EmpId = 1, EmpName = "Test1", Job = "Job1", Salary = 1500},
+                new Employee {EmpId = 2, EmpName = "Test2", Job = "Job2", Salary = 2500},
+                new Employee {EmpId = 3, EmpName = "Test3", Job = "Job3", Salary = 3500},
+                new Employee {EmpId = 4, EmpName = "Test4", Job = "Job4", Salary = 4500},
+                new Employee {EmpId = 5, EmpName = "Test5", Job = "Job4", Salary = 4500},
+                new Employee {EmpId = 6, EmpName = "Test6", Job = "Job4", Salary = 4500},
+                new Employee {EmpId = 7, EmpName = "Test7", Job = "Job4", Salary = 4500},
             };
 
-            var result = productList.Where(prd => prd.Name == "Test1");
+            vm.EmployeeList = employeeList;
 
-            return View(result);
+            return View(vm);
         }
     }
 }
